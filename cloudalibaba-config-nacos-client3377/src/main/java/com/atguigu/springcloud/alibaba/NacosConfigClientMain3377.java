475e54b3540a110ebc754657010ea65d837ed5cd
fuheng7573 <68271608+fuheng7573@users.noreply.github.com>
2022-03-28 14:57:28 +0800

    Add files via upload

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author 付恒
+ * @ClassName NacosConfigClientMain3377
+ * @Date 2020/10/5
+ * @Version 1.0
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
